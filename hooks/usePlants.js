e3d889b0f545a6e777f19692a51f0a5aff0604f6
Pedrito190uwu <mariagaribello190@gmail.com>
2026-06-30 14:14:53 -0500

    fix: crear hook usePlants para gestionar plantas

--- hooks/usePlants.js
+++ hooks/usePlants.js
@@ -4,11 +4,18 @@ export default function usePlants() {
   const [plants, setPlants] = useState([]);
 
   const addPlant = (plant) => {
-    setPlants((prevPlants) => [...prevPlants, plant]);
+    setPlants((currentPlants) => [...currentPlants, plant]);
+  };
+
+  const removePlant = (id) => {
+    setPlants((currentPlants) =>
+      currentPlants.filter((plant) => plant.id !== id)
+    );
   };
 
   return {
     plants,
     addPlant,
+    removePlant,
   };
 }
